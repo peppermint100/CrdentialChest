b9c28d5fbf5a36771dc5d67b5d28b7ac53dcc79d
peppermint100 <krpeppermint100@gmail.com>
2024-06-15 20:20:19 +0900

    initial commit with gitignore and swiftui

---
+++ CredentialChest/CredentialChestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CredentialChestApp.swift
+//  CredentialChest
+//
+//  Created by peppermint100 on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct CredentialChestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
